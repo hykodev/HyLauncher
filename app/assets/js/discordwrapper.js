467b1a5826727911ef2b1327e37d434ae6d9d9bf
HyKo <happy.plan7983@rt.hyko.dev>
2023-03-05 00:03:56 -0500

    Discord RPC

--- app/assets/js/discordwrapper.js
+++ app/assets/js/discordwrapper.js
@@ -13,7 +13,7 @@ exports.initRPC = function(genSettings, servSettings, initialDetails = 'Waiting 
 
     activity = {
         details: initialDetails,
-        state: 'Server: ' + servSettings.shortId,
+        state: servSettings.largeImageText,
         largeImageKey: servSettings.largeImageKey,
         largeImageText: servSettings.largeImageText,
         smallImageKey: genSettings.smallImageKey,
